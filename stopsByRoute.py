2ac978268b9d3ff3691926a0fc8f4dbca654c5f8
zhuang96 <zoehuang96@gmail.com>
2018-03-27 00:24:40 -0400

    Update stopsByRoute.py

--- stopsByRoute.py
+++ stopsByRoute.py
@@ -1,18 +1,24 @@
 import json
 import requests
+import urllib
     
 stopsList=[]
 #get route name from JSON
 def getRoute():
     routeName=parseRoute.route
+    # routeName = "Red"
+    return routeName
 #get direction name from JSON
 def getDirection():
     directionName=parseRoute.direction
+    # directionName="Inbound"
+    return directionName
 
 #create list of stop names
 def getStops():
-	while True:
-		stopsList=parseStops["data"]["attributes"]["name"]
+	for i in range(len(parseStops["data"])):
+		data=parseStops["data"][i]["attributes"]["name"]
+		stopsList.append(data)
 
 def getOrder(direction):
 	zero=["Outbound","Southbound","Westbound","South Station"]
@@ -21,20 +27,26 @@ def getOrder(direction):
 		return stopsList
 	if direction in one:
 		return stopsList[::-1]
+
 #load and get Route & Direction
-routeURL= None  #insert front end JSON
-parseRoute= requests.get(routeURL)
+# routeURL= None  #insert front end JSON
+# routejson = urllib.urlopen(stopsURL)
+# parseRoute= json.loads(routejson.read())
 
 
 #get Route from prev JSON, create stopsByRoute url, load 
-stopsURL= "https://api-v3.mbta.com/schedules?filter%5B"+str(getRoute())
-parseStops=requests.get(stopsURL)
+stopsURL= "https://api-v3.mbta.com/stops?filter%5Broute%5D="+str(getRoute())
+# parseStops=requests.get(stopsURL)
+stopsjson = urllib.urlopen(stopsURL)
+# array=json.dumps(jsonurl)
+parseStops = json.loads(stopsjson.read())
 
 getStops()
 direction=getDirection()
 returnStops=getOrder(direction)
 
-exportStops=json.dump(returnStops)
+exportStops=json.dumps(returnStops)
+print exportStops
 
 
 
