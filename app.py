9314f574d396c09a51a100dcfc425e9517892613
zhuang96 <zoehuang96@gmail.com>
2018-04-10 00:24:50 -0400

    prototype

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def index():
 def sendStops():
 		jsonFront = request.get_json()
 		printthis=json.dumps(jsonFront)
-		print(printthis)
 		route = jsonFront["route"]
 		direction = jsonFront["direction"]
 
@@ -29,55 +28,37 @@ def sendStops():
 		
 		global exportStops
 		exportStops = json.dumps(returnStops)
-		print(exportStops)
 		return jsonify(exportStops)
 # when request for stop prediction is sent
 
-@app.route('/prediction', methods=['POST','GET'])
+@app.route('/prediction', methods=['POST'])
 def sendPredictions():
+    jsonFront = request.get_json()
+    printthis=json.dumps(jsonFront)
+    stop = jsonFront["stop"]
+    route = jsonFront["route"]
+    direction = jsonFront["direction"]
+    direction_id = getDirectionID(direction)
 
-	if request.method == 'POST':
-		# load and get Stop, Route, & Direction
-		jsonFront = request.get_json()
-
-		json.dumps(jsonFront)
-		# print (jsonFront)
-		stop = jsonFront["stop"]
-		route = jsonFront["route"]
-		direction = jsonFront["direction"]
-		direction_id = getDirectionID(direction)
-
-		# load and get all predictions for stop
-		predictionURL = "https://api-v3.mbta.com/predictions?filter[stop]={}".format(stop)
-		predictionDATA = urllib.urlopen(predictionURL)
-		predictionJSON = json.loads(predictionDATA.read())
-
-		predictionURL = "https://api-v3.mbta.com/predictions?filter[stop]={}".format(stop)
-		print (predictionURL)
-		predictionDATA = urllib.urlopen(predictionURL)
-		predictionJSON = json.loads(predictionDATA.read())
-		for i in range(len(predictionJSON["data"])):
-			if (predictionJSON["data"][i]["relationships"]["route"] == route and
-				predictionJSON["data"][i]["attributes"]["direction_id"] == direction_id):
-				depart_time = predictionJSON["data"][i]["attributes"]["departure_time"]
-				arrive_time = predictionJSON["data"][i]["attributes"]["arrival_time"]
-
-		data = {
-			"departure_time": depart_time,
-			"arrival_time": arrive_time
-		}
-		global exportPrediction
-		exportPrediction = json.dumps(data)
-		return Response(exportPrediction, mimetype='application/json')
-
-	print("ELSE")
-	print(exportPrediction)
-	return Response(json.dumps(exportPrediction), mimetype='application/json')
+    predictionURL = "https://api-v3.mbta.com/predictions?filter[stop]={}".format(stop)
+    predictionDATA = urllib.urlopen(predictionURL)
+    predictionJSON = json.loads(predictionDATA.read())
+    counter = 0
+    dataList = []
+    for i in range(len(predictionJSON["data"])):
+        if (predictionJSON["data"][i]["relationships"]["route"]["data"]["id"] == route and
+            predictionJSON["data"][i]["attributes"]["direction_id"] == direction_id) and counter<=3:
+            counter +=1
+            dataDict = {}
+            dataDict["Departure"] = predictionJSON["data"][i]["attributes"]["departure_time"]
+            dataDict["arrive_time"] = predictionJSON["data"][i]["attributes"]["arrival_time"]
+            dataList.append(dataDict)
+    global exportPrediction
+    exportPrediction = json.dumps(dataList)
+    return jsonify(exportPrediction)
 
 def getStops(stopsJSON, direction):
 	stopsList = []
-	# stopsidList= []
-	stopsDict = {}
 	for i in range(len(stopsJSON["data"])):
 	    stopsDict = {}
 	    name = stopsJSON["data"][i]["attributes"]["name"]
@@ -86,7 +67,6 @@ def getStops(stopsJSON, direction):
 	    stopsDict["id"]=stop_id
 	    stopsList.append(stopsDict)
 	    getOrder(stopsList, direction)
-#	stopsDict["data"]=str(stopsList)
 	return stopsList
 
 def getOrder(stopsList, direction):
